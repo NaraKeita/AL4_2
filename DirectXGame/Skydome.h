3cb35d072af6424d4f9c80f79d6b4db563dbf9db
ならけいた <146915043+NaraKeita@users.noreply.github.com>
2024-12-12 10:10:50 +0900

    途中

---
+++ DirectXGame/Skydome.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "WorldTransform.h"
+#include "Model.h"
+
+
+class Skydome {
+public:
+	//<summary>
+	//初期化
+	//</summary>
+	void Initialize(Model* model, ViewProjection* viewProjection);
+
+	//<summary>
+	//更新
+	//</summary>
+	void Update();
+
+	//<summary>
+	//描画
+	//</summary>
+	void Draw();
+
+	private:
+		//ワールド変換データ
+	    WorldTransform worldTransform_;
+		//モデル
+	    Model* model_ = nullptr;
+
+		Model* modelSkydome_ = nullptr;
+	    // テクスチャハンドル
+	   
+		ViewProjection* viewProjection_ = nullptr;
+};
