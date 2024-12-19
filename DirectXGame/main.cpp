5c0dd11b9bd0200bdc57cc5f34707a9ff6fedb62
ならけいた <146915043+NaraKeita@users.noreply.github.com>
2024-12-19 14:22:55 +0900

    シーンを入れている途中

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -102,3 +102,132 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	return 0;
 }
+
+ // シーン切り替え
+// シーン切り替え
+void ChangeScene() {
+
+	switch (scene) {
+	case Scene::kTitle:
+		if (titleScene->IsFinished()) {
+			// シーン変更
+			scene = Scene::kMobGame;
+			// 旧シーンの開放
+			delete titleScene;
+			titleScene = nullptr;
+			// 新シーンの生成と初期化
+			mobScene = new MobScene;
+			mobScene->Initialize();
+		}
+		break;
+	case Scene::kMobGame:
+		if (mobScene->EnemyIsFinished()) {
+			// シーン変更
+			scene = Scene::kGame;
+			// 旧シーンの開放
+			delete mobScene;
+			mobScene = nullptr;
+			// 新シーンの生成と初期化
+			gameScene = new GameScene;
+			gameScene->Initialize();
+		} else if (mobScene->HP <= 0) {
+			// シーン変更
+			scene = Scene::kDead;
+			// 旧シーンの開放
+			delete mobScene;
+			mobScene = nullptr;
+			// 新シーンの生成と初期化
+			deadScene = new DeadScene;
+			deadScene->Initialize();
+		}
+
+		break;
+	case Scene::kGame:
+		if (gameScene->enemyHP <= 0) {
+			// シーン変更
+			scene = Scene::kClear;
+			// 旧シーンの開放
+			delete gameScene;
+			gameScene = nullptr;
+			// 新シーンの生成と初期化
+			clearScene = new ClearScene;
+			clearScene->Initialize();
+		} else if (gameScene->HP <= 0) {
+			// シーン変更
+			scene = Scene::kDead;
+			// 旧シーンの開放
+			delete gameScene;
+			gameScene = nullptr;
+			// 新シーンの生成と初期化
+			deadScene = new DeadScene;
+			deadScene->Initialize();
+		}
+
+		break;
+	case Scene::kClear:
+		if (clearScene->IsFinished()) {
+			// シーン変更
+			scene = Scene::kTitle;
+			// 旧シーンの開放
+			delete clearScene;
+			clearScene = nullptr;
+			// 新シーンの生成と初期化
+			titleScene = new TitleScene;
+			titleScene->Initialize();
+		}
+		break;
+	case Scene::kDead:
+		if (deadScene->IsFinished()) {
+			// シーン変更
+			scene = Scene::kTitle;
+			// 旧シーンの開放
+			delete deadScene;
+			deadScene = nullptr;
+			// 新シーンの生成と初期化
+			titleScene = new TitleScene;
+			titleScene->Initialize();
+		}
+	}
+}
+
+// 現在シーン更新
+void UpdateScene() {
+	switch (scene) {
+	case Scene::kTitle:
+		titleScene->Update();
+		break;
+	case Scene::kMobGame:
+		mobScene->Update();
+		break;
+	case Scene::kGame:
+		gameScene->Update();
+		break;
+	case Scene::kClear:
+		clearScene->Update();
+		break;
+	case Scene::kDead:
+		deadScene->Update();
+		break;
+	}
+}
+
+// 現在シーンの描画
+void DrawScene() {
+	switch (scene) {
+	case Scene::kTitle:
+		titleScene->Draw();
+		break;
+	case Scene::kMobGame:
+		mobScene->Draw();
+		break;
+	case Scene::kGame:
+		gameScene->Draw();
+		break;
+	case Scene::kClear:
+		clearScene->Draw();
+		break;
+	case Scene::kDead:
+		deadScene->Draw();
+		break;
+	}
+}
